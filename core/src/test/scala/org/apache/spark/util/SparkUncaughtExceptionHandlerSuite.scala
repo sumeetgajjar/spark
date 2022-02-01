20820ddfc69cdc46c5cba680ad685ea69136c891
Sumeet Gajjar <sumeetgajjar93@gmail.com>
2022-01-31 22:23:47 -0800

    Tag failing tests to exclude during code coverage

--- core/src/test/scala/org/apache/spark/util/SparkUncaughtExceptionHandlerSuite.scala
+++ core/src/test/scala/org/apache/spark/util/SparkUncaughtExceptionHandlerSuite.scala
@@ -41,7 +41,7 @@ class SparkUncaughtExceptionHandlerSuite extends SparkFunSuite {
     case (throwable: ThrowableTypes.ThrowableTypesVal,
     exitOnUncaughtException: Boolean, expectedExitCode) =>
       test(s"SPARK-30310: Test uncaught $throwable, " +
-          s"exitOnUncaughtException = $exitOnUncaughtException") {
+          s"exitOnUncaughtException = $exitOnUncaughtException", ExcludeForCodeCoverage) {
 
         // creates a ThrowableThrower process via spark-class and verify the exit code
         val process = Utils.executeCommand(
